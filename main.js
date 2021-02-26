958824836a34b92fff574703d1f62e7583756d71
sergeysedoy97 <sergey.sedoy.97@icloud.com>
2021-02-26 14:37:59 +0300

    Release 0.2.0

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-import { createReadStream, promises } from 'fs'
-import { resolve as getAbsolutePath, extname as getExtension } from 'path'
-
-import { STATUS_CODES } from 'http'
-import { createHash } from 'crypto'
-
-const { stat: getStats } = promises // Because fs/promises causes problems in old versions of Node.js
+/* eslint-disable node/no-unsupported-features/es-syntax */
 
 // REFERENCES
 // https://tools.ietf.org/html/rfc7230#section-3
@@ -26,8 +20,6 @@ const STATUS_OK = 200
 const STATUS_NOT_MODIFIED = 304
 const STATUS_NOT_FOUND = 404
 const STATUS_PRECONDITION_FAILED = 412
-const DEFAULT_CONTENT_TYPE = 'application/octet-stream'
-const DEFAULT_DIRECTORY_PATH = getAbsolutePath('.')
 const DEFAULT_CONTENT_TYPE_BY_EXTENSIONS = new Map([
   ['.css', 'text/css'],
   ['.html', 'text/html'],
@@ -45,75 +37,11 @@ const DEFAULT_CONTENT_TYPE_BY_EXTENSIONS = new Map([
   ['.woff2', 'font/woff2'],
   ['.xml', 'application/xml'],
 ])
+const DEFAULT_CONTENT_TYPE = 'application/octet-stream'
+const DEFAULT_DIRECTORY_PATH = '.'
 const DEFAULT_USE_WEAK_ETAGS = true
 
-const generateHeaders = ({ file }) => {
-  return {
-    'Cache-Control': 'public',
-    'Content-Length': file.contentLength,
-    'Content-Type': file.contentType,
-    'ETag': file.eTag,
-    'Last-Modified': file.lastModified,
-  }
-}
-
-const generateWeakETag = ({ stats: { mtime } }) => {
-  const value = mtime.valueOf().toString(32)
-  return `W/"${value}"`
-}
-
-const generateStrongETag = ({ file }) => {
-  return new Promise((resolve) => {
-    const hash = createHash('blake2b512')
-    createReadStream(file.absolutePath)
-      .on('close', () => {
-        const value = hash.read().toString('base64')
-        resolve(`"${value}"`)
-      })
-      .pipe(hash)
-  })
-}
-
-const preconditionFailed = ({ response }) => {
-  response
-    .writeHead(STATUS_PRECONDITION_FAILED, STATUS_CODES[STATUS_PRECONDITION_FAILED], {
-      'Cache-Control': 'no-store',
-    })
-    .end()
-}
-
-const notFound = ({ response }) => {
-  response
-    .writeHead(STATUS_NOT_FOUND, STATUS_CODES[STATUS_NOT_FOUND], {
-      'Cache-Control': 'no-store',
-    })
-    .end()
-}
-
-const notModified = ({ file, response }) => {
-  response
-    .writeHead(
-      STATUS_NOT_MODIFIED,
-      STATUS_CODES[STATUS_NOT_MODIFIED],
-      generateHeaders({
-        file,
-      })
-    )
-    .end()
-}
-
-const ok = ({ file, response }) => {
-  response.writeHead(
-    STATUS_OK,
-    STATUS_CODES[STATUS_OK],
-    generateHeaders({
-      file,
-    })
-  )
-  createReadStream(file.absolutePath).pipe(response)
-}
-
-export const staticHandler = (
+export const staticHandler = async (
   {
     contentTypeByExtensions = DEFAULT_CONTENT_TYPE_BY_EXTENSIONS,
     defaultContentType = DEFAULT_CONTENT_TYPE,
@@ -126,69 +54,130 @@ export const staticHandler = (
     useWeakETags: DEFAULT_USE_WEAK_ETAGS,
   }
 ) => {
-  return {
-    async handle({ request, response }) {
-      let isAborted = false
-      request.once('aborted', () => {
-        isAborted = true
+  const crypto = await import('crypto')
+  const fs = await import('fs')
+  const path = await import('path')
+  const generateHeaders = ({ file }) => {
+    return {
+      'Cache-Control': 'public',
+      'Content-Length': file.contentLength,
+      'Content-Type': file.contentType,
+      'ETag': file.eTag,
+      'Last-Modified': file.lastModified,
+    }
+  }
+  const generateWeakETag = ({ stats: { mtime } }) => {
+    const value = mtime.valueOf().toString(32)
+    return `W/"${value}"`
+  }
+  const generateStrongETag = ({ file }) => {
+    return new Promise((resolve) => {
+      const hash = crypto.createHash('blake2b512')
+      fs.createReadStream(file.absolutePath)
+        .on('close', () => {
+          const value = hash.read().toString('base64')
+          resolve(`"${value}"`)
+        })
+        .pipe(hash)
+    })
+  }
+  const preconditionFailed = ({ response }) => {
+    response
+      .writeHead(STATUS_PRECONDITION_FAILED, {
+        'Cache-Control': 'no-store',
       })
-      const { pathname } = new URL(request.url, `protocol://${request.headers.host}`) // WTF Node.js? https://nodejs.org/api/http.html#http_message_url
-      const file = {
-        absolutePath: getAbsolutePath(directoryPath, pathname.substring(1)),
-        contentType: contentTypeByExtensions.get(getExtension(pathname)) ?? defaultContentType,
-        isExists: true,
-      }
-      try {
-        const stats = await getStats(file.absolutePath)
-        if (stats.isFile() === false) {
-          file.isExists = false
-        } else {
-          file.contentLength = stats.size
-          file.lastModified = stats.mtime.toUTCString()
-          if (useWeakETags === true) {
-            file.eTag = generateWeakETag({
-              stats,
-            })
-          } else {
-            file.eTag = await generateStrongETag({
-              file,
-            })
-          }
-        }
-      } catch {
+      .end()
+  }
+  const notFound = ({ response }) => {
+    response
+      .writeHead(STATUS_NOT_FOUND, {
+        'Cache-Control': 'no-store',
+      })
+      .end()
+  }
+  const notModified = ({ file, response }) => {
+    response
+      .writeHead(
+        STATUS_NOT_MODIFIED,
+        generateHeaders({
+          file,
+        })
+      )
+      .end()
+  }
+  const ok = ({ file, response }) => {
+    response.writeHead(
+      STATUS_OK,
+      generateHeaders({
+        file,
+      })
+    )
+    fs.createReadStream(file.absolutePath).pipe(response)
+  }
+  const handle = async ({ request, response }) => {
+    let isAborted = false
+    request.once('aborted', () => {
+      isAborted = true
+    })
+    const pathname = request.url.includes('?') === true ? request.url.substring(0, request.url.indexOf('?')) : request.url
+    const file = {
+      absolutePath: path.resolve(directoryPath, pathname.substring(1)),
+      contentType: contentTypeByExtensions.get(path.extname(pathname)) ?? defaultContentType,
+      isExists: true,
+    }
+    try {
+      const stats = await fs.promises.stat(file.absolutePath)
+      if (stats.isFile() === false) {
         file.isExists = false
-      }
-      if (isAborted === true) {
-        return
-      } else if (file.isExists === false) {
-        notFound({
-          response,
-        })
-      } else if (
-        ('if-match' in request.headers === true && request.headers['if-match'] !== file.eTag) ||
-        ('if-match' in request.headers === false &&
-          'if-unmodified-since' in request.headers === true &&
-          request.headers['if-unmodified-since'] < file.lastModified)
-      ) {
-        preconditionFailed({
-          response,
-        })
-      } else if (
-        ('if-none-match' in request.headers === true && request.headers['if-none-match'] === file.eTag) ||
-        ('if-none-match' in request.headers === false &&
-          'if-modified-since' in request.headers === true &&
-          request.headers['if-modified-since'] >= file.lastModified)
-      ) {
-        notModified({
-          file,
-          response,
-        })
       } else {
-        ok({
-          file,
-          response,
-        })
+        file.contentLength = stats.size
+        file.lastModified = stats.mtime.toUTCString()
+        if (useWeakETags === true) {
+          file.eTag = generateWeakETag({
+            stats,
+          })
+        } else {
+          file.eTag = await generateStrongETag({
+            file,
+          })
+        }
       }
-    },
+    } catch {
+      file.isExists = false
+    }
+    if (isAborted === true) {
+      return
+    } else if (file.isExists === false) {
+      notFound({
+        response,
+      })
+    } else if (
+      ('if-match' in request.headers === true && request.headers['if-match'] !== file.eTag) ||
+      ('if-match' in request.headers === false &&
+        'if-unmodified-since' in request.headers === true &&
+        request.headers['if-unmodified-since'] < file.lastModified)
+    ) {
+      preconditionFailed({
+        response,
+      })
+    } else if (
+      ('if-none-match' in request.headers === true && request.headers['if-none-match'] === file.eTag) ||
+      ('if-none-match' in request.headers === false &&
+        'if-modified-since' in request.headers === true &&
+        request.headers['if-modified-since'] >= file.lastModified)
+    ) {
+      notModified({
+        file,
+        response,
+      })
+    } else {
+      ok({
+        file,
+        response,
+      })
+    }
+  }
+  return {
+    handle,
   }
 }
--- test/all.test.js
+++ test/all.test.js
@@ -1,38 +1,44 @@
-import { promises as fs } from 'fs'
-import { http } from '@awesomeorganization/servers'
-import { ok } from 'assert'
-import { staticHandler } from '../main.js'
-import undici from 'undici'
+/* eslint-disable node/no-unsupported-features/es-syntax */
 
-const main = async () => {
-  const host = '127.0.0.1'
-  const port = 3000
-  const { handle } = staticHandler()
-  const socket = await http({
-    host,
+const test = async () => {
+  const { staticHandler } = await import('../main.js')
+  const { deepStrictEqual } = await import('assert')
+  const { http } = await import('@awesomeorganization/servers')
+  const {
+    promises: { readFile },
+  } = await import('fs')
+  const {
+    default: { Client },
+  } = await import('undici')
+  const { handle } = await staticHandler()
+  http({
+    listenOptions: {
+      host: '127.0.0.1',
+      port: 0,
+    },
+    async onListening() {
+      const filename = 'main.js'
+      const { address, port } = this.address()
+      const { body } = await new Client(`http://${address}:${port}`).request({
+        method: 'GET',
+        path: `/${filename}`,
+      })
+      const chunks = []
+      body.on('data', (chunk) => {
+        chunks.push(chunk)
+      })
+      body.once('end', async () => {
+        deepStrictEqual(Buffer.concat(chunks), await readFile(`./${filename}`))
+        this.close()
+      })
+    },
     async onRequest(request, response) {
       await handle({
         request,
         response,
       })
     },
-    port,
   })
-  {
-    const path = '/main.js'
-    const { body } = await new undici.Client(`http://${host}:${port}`).request({
-      method: 'GET',
-      path,
-    })
-    const chunks = []
-    body.on('data', (chunk) => {
-      chunks.push(chunk)
-    })
-    body.on('end', async () => {
-      ok(Buffer.concat(chunks).equals(await fs.readFile('.' + path)))
-    })
-  }
-  socket.unref()
 }
 
-main()
+test()
